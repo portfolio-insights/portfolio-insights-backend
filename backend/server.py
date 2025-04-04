6ec4975bcb28fcff8a0cf1cd3eea1ead22d6d6b2
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-04 08:49:57 -0700

    Create a new path in server.py to print the name of the desired ticker using path parameters

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@
 Development begun using the FastAPI "First Steps" tutorial.
 Reference: https://fastapi.tiangolo.com/tutorial/first-steps/
 
+Start a server by running the shell command: fastapi dev server.py
 
 '''
 
@@ -9,10 +10,11 @@ from fastapi import FastAPI
 
 app = FastAPI() # Initialize FastAPI server
 
-@app.get("/") # Root endpoint
+@app.get('/') # Root endpoint
 async def root():
-    return {"message": "Hello World"}
+    return 'Hello World'
 
-@app.get("/test_yfinance") # Endopoint to test yfinance functionality
-async def root():
-    return {"message": "This is an endpoint where we will test yfinance functionality."}
+@app.get("/data/{ticker}")
+async def historical_data(ticker = 'SPY'): # Endpoint to practice using path parameters
+    if len(ticker) > 5: return 'Error! Invalid Ticker!'
+    return ticker
