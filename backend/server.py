d910069e4c1f4bf78eff294d1aec328e560b99c7
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-04 09:04:44 -0700

    Connect server.py with yfinance_functions.py

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,13 @@
 Development begun using the FastAPI "First Steps" tutorial.
 Reference: https://fastapi.tiangolo.com/tutorial/first-steps/
 
+Activate the backend virtual environment by running the shell command: source venv/bin/activate
 Start a server by running the shell command: fastapi dev server.py
 
 '''
 
 from fastapi import FastAPI
+from yfinance_functions import historical_data
 
 app = FastAPI() # Initialize FastAPI server
 
@@ -17,7 +19,7 @@ async def root():
 @app.get("/data/{ticker}")
 async def historical_data_p(ticker = 'SPY'): # Endpoint to practice using path parameters
     if len(ticker) > 5: return 'Error! Invalid Ticker!'
-    return ticker
+    return historical_data(ticker)
 
 @app.get("/data")
 async def historical_data_q(ticker = 'SPY'): # Same functionality as above; endpoint to practice using query parameters
---
+++ backend/yfinance_functions.py
@@ -0,0 +1,23 @@
+'''
+Development begun using AlgoTrading101 Blog's "yfinance Library – A Complete Guide" article as areference.
+Reference: https://algotrading101.com/learn/yfinance-guide/
+
+'''
+
+# import yfinance as yf
+
+# apple= yf.Ticker("aapl")
+
+# show actions (dividends, splits)
+# apple.actions
+
+# show dividends
+# apple.dividends
+
+# show splits
+# apple.splits
+
+# + other methods etc.
+
+def historical_data(ticker):
+  return f'You\'re retrieving historical data for {ticker}!'
