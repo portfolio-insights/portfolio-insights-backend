8ea407e0dbbfe8d925260a7c66afb068a4758ba0
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-06 17:10:25 -1000

    Add skeleton code for server.py routes interacting with alerts.py

--- backend/server.py
+++ backend/server.py
@@ -8,7 +8,7 @@ Start a server by running the shell command: fastapi dev server.py
 '''
 
 from fastapi import FastAPI
-from yfinance_functions import stock_info
+import alerts
 
 app = FastAPI() # Initialize FastAPI server
 
@@ -18,8 +18,26 @@ async def root():
 
 @app.get("/info/{ticker}")
 async def get_stock_info(ticker = 'SPY'): # Endpoint to return basic stock information
-    if len(ticker) > 5: return 'Error! Invalid Ticker!'
     return stock_info(ticker)
+
+# ------------------------------------------------------------------------ #
+
+##### Managing Alerts #####
+@app.get("/alerts")
+def get_alert(id): # Endpoint to retrieve an alert by id
+    return alerts.get(id)
+
+@app.post("/alerts")
+def create_alert(ticker, price, direction, one_time, expiration_date): # Endpoint to create a new alert
+    return alerts.create(ticker, price, direction, one_time, expiration_date)
+
+@app.delete("/alerts")
+def delete_alert(id): # Endpoint to delete an existing alert by id
+    return alerts.delete(id)
+
+@app.patch("/alerts")
+def update_alert(id, ticker, price, direction, one_time, expiration_date): # Endpoint to return basic stock information
+    return alerts.update(id)
 
 '''
 @app.get("/data")
