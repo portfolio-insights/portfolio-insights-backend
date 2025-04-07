61d43481d972281da83886cb784e1baa9a0ba246
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-07 12:48:11 -1000

    Replace ''' with """ for docstrings to follow PEP 257

--- backend/yfinance_functions.py
+++ backend/yfinance_functions.py
@@ -1,11 +1,11 @@
-'''
+""""""
 Development begun using AlgoTrading101 Blog's "yfinance Library – A Complete Guide" article as a reference:
 https://algotrading101.com/learn/yfinance-guide/
 
 Full documentation available here:
 https://yfinance-python.org/
 
-'''
+"""
 
 import yfinance as yf
 
