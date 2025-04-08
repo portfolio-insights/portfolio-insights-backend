396fb3793067331f2439f0c537d2fe2ae2b8e87a
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-07 21:26:25 -1000

    Add functionality for evaluating and triggering alerts

--- backend/alerts.py
+++ backend/alerts.py
@@ -3,6 +3,8 @@ This file implements all of the functionality in order to manage stock price ale
 """
 
 import database
+import market
+from datetime import datetime
 
 def get(id):
   """
@@ -36,3 +38,37 @@ def update(id):
   """
   Update a stock price alert by id.
   """
+
+def evaluate():
+  """
+  Evaluate all alerts against stock prices to determine if alert should be triggered.
+  """
+  # Retrieve alerts from database
+  with database.connection.cursor() as cur:
+    cur.execute('SELECT alert_id, ticker, price, direction FROM alerts;')
+    alerts = cur.fetchall()
+
+  # Evaluate alerts and trigger as appropriate
+  checked = {} # Dictionary for caching checked alerts with associated price
+  for id, ticker, price, direction in alerts: # Iterate through returned alerts
+    # Retrieve stock price
+    if ticker in checked: # Stock price has already been retreived
+      stock_price = checked[ticker] # Retrieve cached stock price
+    else: # Stock price has not yet been retreived
+      stock_price = market.stock_price(ticker) # Retrieve current stock price using yfinance
+      checked[ticker] = stock_price # Cache current stock price
+
+    # Check alert price against actual stock price
+    if (direction == 'below' and price > stock_price) or (direction == 'above' and price < stock_price):
+      trigger(id)
+
+def trigger(id):
+  """
+  Triggers an alert based on its id.
+  """
+  with database.connection.cursor() as cur:
+    cur.execute('''
+                UPDATE alerts
+                SET triggered = true, triggered_time = %s
+                WHERE alert_id = %s;
+                ''', (datetime.now(), id))
