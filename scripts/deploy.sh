40137aa2e1dfbbb1c693dd894a85da230a386392
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-05-23 00:33:50 -0700

    chore: create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ""
+echo "🛑 Stopping old Docker container..."
+docker stop portfolio-insights-backend || true
+echo "✅ Done."
+
+echo ""
+echo "🧼 Removing old Docker container and images..."
+docker rm portfolio-insights-backend || true
+docker rmi portfolio-insights-backend || true
+echo "✅ Done."
+
+echo ""
+echo "🐳 Building new Docker image..."
+docker build --pull -t portfolio-insights-backend .
+echo "✅ Done."
+
+echo ""
+echo "🚀 Spinning up new Docker container..."
+docker run -d --restart unless-stopped --name portfolio-insights-backend -p 8001:8001 --env-file .env portfolio-insights-backend
+echo "✅ Done."
+
+echo ""
+echo "🎉 Backend running on Docker."
